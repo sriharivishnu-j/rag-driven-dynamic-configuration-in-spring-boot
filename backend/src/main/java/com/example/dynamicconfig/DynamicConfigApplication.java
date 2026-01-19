3e8393f9540896bfea207ddc03db5fe3b32017a8
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-19 12:39:12 +0800

    Add backend/src/main/java/com/example/dynamicconfig/DynamicConfigApplication.java

---
+++ backend/src/main/java/com/example/dynamicconfig/DynamicConfigApplication.java
@@ -0,0 +1,12 @@
+package com.example.dynamicconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicConfigApplication.class, args);
+    }
+}
